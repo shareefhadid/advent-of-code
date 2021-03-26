cda41bdbf26ee571ad4c663f42813f1d5a6a0f59
Shareef Hadid <shareefhadid@gmail.com>
2021-03-26 03:52:16 -0400

    fixed comments

--- day-4/day-4.js
+++ day-4/day-4.js
@@ -1,19 +1,22 @@
 const fs = require('fs')
 const input = fs.readFileSync('./input.txt', 'utf-8')
 
-// Turn intput into one dimensional string. Get rid of new lines and returns from encoding.
+// Turn intput into array of passports
 let inputArray = input.replace(/\r/ig, "").split("\n\n").map(val => val.replace(/[\n\+]/ig, " "))
-let required = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
 
+// initialize required fields
+let required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+
+// Validates if array of passport contains all of the require fields
 function passportValidator(arrayOfPassports, arrayRequired) {
-    let counter = 0
-    arrayOfPassports.forEach(passport => {
-      let valid = true
-      for (const requirement of arrayRequired) {
-        if (!passport.includes(requirement)) valid = false
-      }
-      if (valid) counter++
-    })
-    return counter
-  }
+  let counter = 0
+  arrayOfPassports.forEach(passport => {
+    let valid = true
+    for (const requirement of arrayRequired) {
+      if (!passport.includes(requirement)) valid = false
+    }
+    if (valid) counter++
+  })
+  return counter
+}
 console.log(passportValidator(inputArray, required))
