14527ffa7a1fd687183841525ee8f6beb5c3149a
Shareef Hadid <shareefhadid@gmail.com>
2021-03-26 00:05:03 -0400

    .

--- day-2/day-2.js
+++ day-2/day-2.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const input = fs.readFileSync('./input.txt','utf-8')
 
-let inputArray = input.split("\n").map((string) => Number(string))
+let inputArray = input.split("\n")
 
 console.log(inputArray)
