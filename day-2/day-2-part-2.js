a0b2bfffc5027f469dd543c65611d117bbe0f6fd
Shareef Hadid <shareefhadid@gmail.com>
2021-03-26 01:07:14 -0400

    solved day-2-part-2, might want to refactor/look for a better solution

---
+++ day-2/day-2-part-2.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const input = fs.readFileSync('./input.txt', 'utf-8')
+
+// Create an array of objects. Each object will store the password policy details and password
+let inputArray = input.split("\n").map(function (string) {
+  let substr = string.replace(/[-:\r]/ig, " ").split(" ")
+  return {
+    firstIndex: Number(substr[0])-1,
+    secondIndex: Number(substr[1])-1,
+    letter: substr[2],
+    password: substr[4],
+  }
+})
+
+function countValidPasswords(arrayOfPasswords) {
+  let validCount = 0
+  arrayOfPasswords.forEach(function ({ firstIndex, secondIndex, letter, password }) {
+    let count = 0
+    if (password[firstIndex]== letter) count ++
+    if (password[secondIndex]== letter) count ++
+    if (count == 1) validCount++
+  })
+  return validCount
+}
+
+console.log(countValidPasswords(inputArray))
