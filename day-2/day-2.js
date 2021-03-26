1c65e3a08b3de9ff871815f47c7741326322f93f
Shareef Hadid <shareefhadid@gmail.com>
2021-03-26 00:38:17 -0400

    day-2: Processed input

--- day-2/day-2.js
+++ day-2/day-2.js
@@ -1,6 +1,13 @@
 const fs = require('fs')
-const input = fs.readFileSync('./input.txt','utf-8')
+const input = fs.readFileSync('./input.txt', 'utf-8')
 
-let inputArray = input.split("\n")
-
-console.log(inputArray)
+// Create an array of objects. Each object will store the password policy details and password
+let inputArray = input.split("\n").map(function (string) {
+  let substr = string.replace(/[-:\r]/ig, " ").split(" ")
+  return {
+    min: substr[0],
+    max: substr[1],
+    letter: substr[2],
+    password: substr[4],
+  }
+})
